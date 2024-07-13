33cd40500048ec3b7d96e5038bcee8e01184cfaa
IamNormi <automatecode@gmail.com>
2024-07-13 15:35:55 +0530

    tab count to 123

--- config.h
+++ config.h
@@ -45,7 +45,10 @@ static char *colors[][3] = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "󰣛", "", ""  };
+// static const char *tags[] = { "", "", "", "", "", "", "󰣛", "", ""  };
+
+static const char *tags[] = { "", "", "" };
+
 
 static const Rule rules[] = {
 	/* xprop(1):
